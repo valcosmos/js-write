b0759bda7c91217a7c7107bf235fab16ee3457fe
Cupid Valentine <westerngod@126.com>
2022-04-29 23:05:58 +0800

    add bubble-sort

---
+++ bubble-sort.js
@@ -0,0 +1,31 @@
+const arr = [12, 2, 23, 43, 43, 5, 45, 4, 54, 65, 65656]
+
+function bubbleSort(arr) {
+  let temp
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+const bubbleSort1 = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+
+  return arr
+}
+console.log(bubbleSort1(arr))
